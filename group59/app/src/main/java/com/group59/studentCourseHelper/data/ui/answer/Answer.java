f10ad26f3a798a19b4b0d8be2291d587f2fbb3c5
Huang Peiyu <hpy9707@gmail.com>
2019-10-23 01:03:19 +1100

    Answer--basic logic done

---
+++ group59/app/src/main/java/com/group59/studentCourseHelper/data/ui/answer/Answer.java
@@ -0,0 +1,14 @@
+package com.group59.studentCourseHelper.data.ui.answer;
+
+public class Answer {
+    private String answer;
+    public Answer(){}
+
+    public Answer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+}
--- group59/app/src/main/java/com/group59/studentCourseHelper/data/ui/answer/answerActivity.java
+++ group59/app/src/main/java/com/group59/studentCourseHelper/data/ui/answer/answerActivity.java
@@ -3,34 +3,67 @@ package com.group59.studentCourseHelper.data.ui.answer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.group59.studentCourseHelper.R;
+import com.group59.studentCourseHelper.data.ui.post.Question;
 
 public class answerActivity extends AppCompatActivity {
-    TextView query;
+    //TextView answer;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-
-    DatabaseReference myRef = database.getReference("users");
+    Button ans_submit;
+    Button ans_back;
+    EditText answer;
+    DatabaseReference myRef = database.getReference("answers");
     private String TAG = getClass().getName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer);
-        query = findViewById(R.id.text_query);
-        Intent intent = getIntent();
-        String a = intent.getStringExtra("title");
-        Log.i(TAG, "onCreateView: :"+a);
-        query.setText(a);
-        showView(a);
+        answer = findViewById(R.id.anws);
+        ans_back=(Button)findViewById(R.id.ans_back);
+        ans_submit=(Button)findViewById(R.id.ans_submit);
+        ans_submit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+               if( addans()){
+                   finish();
+               }
+            }
+        });
+        ans_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+//        Intent intent = getIntent();
+//        String a = intent.getStringExtra("title");
+//        Log.i(TAG, "onCreateView: :"+a);
+//        answer.setText(a);
+//        showView(a);
+
     }
 
     private void showView(String a) {
 
     }
+    private boolean addans(){
+    String m_answer=answer.getText().toString().trim();
+        String id= myRef.push().getKey();
+        Answer m_ans=new Answer(m_answer);
+        myRef.child(id).setValue(m_ans);
+        Toast.makeText(this,"You have posted an answer succefully",Toast.LENGTH_LONG).show();
+        return true;
 
+    }
 }
