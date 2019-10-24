f386839385e61d7f326179df408853f5cc751b77
guoy7 <guoy7@student.unimelb.edu.au>
2019-10-24 13:36:42 +1100

    search

--- group59/app/src/main/java/com/group59/studentCourseHelper/data/ui/answer/AnswerActivity.java
+++ group59/app/src/main/java/com/group59/studentCourseHelper/data/ui/answer/AnswerActivity.java
@@ -9,10 +9,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.group59.studentCourseHelper.R;
 import com.group59.studentCourseHelper.data.ui.post.Question;
 
@@ -22,15 +27,37 @@ public class AnswerActivity extends AppCompatActivity {
     Button ans_submit;
     Button ans_back;
     EditText answer;
+    String name;
+    String qid;
     DatabaseReference myRef = database.getReference("answers");
+    DatabaseReference userRef=database.getReference("users");
+    FirebaseAuth mAuth=FirebaseAuth.getInstance();
     private String TAG = getClass().getName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        userRef.child(mAuth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String a=dataSnapshot.child("name").getValue(String.class);
+                name=a;
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });;
         setContentView(R.layout.activity_answer);
         answer = findViewById(R.id.answ);
         ans_back=(Button)findViewById(R.id.ans_back);
         ans_submit=(Button)findViewById(R.id.ans_submit);
+        Intent intent = getIntent();
+        String a = intent.getStringExtra("qid");
+        qid=a;
+        Log.i(TAG, "onAnswer: :"+qid);
+
+
         ans_submit.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -46,11 +73,7 @@ public class AnswerActivity extends AppCompatActivity {
             }
         });
 
-        Intent intent = getIntent();
-        String a = intent.getStringExtra("title");
-        Log.i(TAG, "onCreateView: :"+a);
-        answer.setText(a);
-        showView(a);
+
 
     }
 
@@ -59,9 +82,10 @@ public class AnswerActivity extends AppCompatActivity {
     }
     private boolean addans(){
     String m_answer=answer.getText().toString().trim();
-        String id= myRef.push().getKey();
-        Answer m_ans=new Answer(m_answer);
-        myRef.child(id).setValue(m_ans);
+        String aid= myRef.push().getKey();
+        String id= qid;
+        Answer m_ans=new Answer(m_answer,aid,qid,name);
+        myRef.child(qid).child(aid).setValue(m_ans);
         Toast.makeText(this,"You have posted an answer succefully",Toast.LENGTH_LONG).show();
         return true;
 
--- group59/app/src/main/java/com/group59/studentCourseHelper/data/ui/search/SearchFragment.java
+++ group59/app/src/main/java/com/group59/studentCourseHelper/data/ui/search/SearchFragment.java
@@ -79,11 +79,12 @@ public class SearchFragment extends Fragment {
    public void Event(){
         searchList = new ArrayList();
        final String input = InputSearch.getText().toString();
-       myRef.orderByChild("questionTitle").addValueEventListener(new ValueEventListener() {
+       myRef.child("8CMYneZckXOzjUMWQRKEoxDrmTF3").orderByChild("tag").addValueEventListener(new ValueEventListener() {
            @Override
            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
-                   final String title = postSnapshot.child(("questionTitle")).getValue().toString();
+                   Log.i(TAG, "onSearch::" +postSnapshot.child("tag"));
+                   final String title = postSnapshot.child("quesionTitle").getValue().toString();
                    final String tag = postSnapshot.child("tag").getValue().toString();
                    final String qid = postSnapshot.child("questionId").getValue().toString();
                    char[] c = input.toCharArray();
@@ -116,7 +117,7 @@ public class SearchFragment extends Fragment {
                Log.i(TAG, "onClick::" +search.getTitle());
 
                Intent intent = new Intent();
-               intent.setAction("android.intent.action.ANSWER");
+               intent.setAction("android.intent.action.DETAILS");
                intent.putExtra("qid",search.getQid());
                startActivity(intent);
            }
